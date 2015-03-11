536cdea7c3acb2d3788d27cb20804611b288567e
Fernando-Guerra <fernando_theotaku@hotmail.com>
2015-03-11 11:03:20 -0430

    practicas resumeBuilder.js
    Practicas realizadas para probar el cambio de cadenas con replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,18 @@
-$("#main").append("Fernando Guerra");
+// $("#main").append("Fernando Guerra");
 
+ 
+/*This replace a string with other
+[string].replace([old], [new])
+
+var email = "fernando.cabezas@loyalfeel.com";
+var newEmail = email.replace("loyalfeel", "cibersys");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "My name in Fernando and I am AWESOME ";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+Selector of Jquery to show the results in the browser
+$("#main").append(funThoughts);
+*/
