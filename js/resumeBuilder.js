8759855fe0914770f91b4b491ce99b1a1127dec3
Fernando-Guerra <fernando_theotaku@hotmail.com>
2015-03-12 10:45:26 -0430

    Practicas de con Arrays y Objetos
    Practicas Realizadas con Arrays y Objetos Verificando su comportamiento
    y como se pueden fusionar ambos

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,8 +17,57 @@ Selector of Jquery to show the results in the browser
 $("#main").append(funThoughts);
 */
 
-var formattedName = HTMLheaderName.replace("%data%", "Fernando Cabezas");
+/*var formattedName = HTMLheaderName.replace("%data%", "Fernando Cabezas");
 var formattedRole = HTMLheaderRole.replace("%data%","Developer");
 
 $("#header").prepend(formattedName);
-$("#header").append(formattedRole);
+$("#header").append(formattedRole);*/
+
+
+
+//$("#main").append(skills);
+
+//$("#main").append(skills[0]);
+
+//$("#main").append(skills.length);
+
+/* Objetos y Comportamiento
+
+var bio = {
+	"name" : "Fernando Cabezas",
+	"role" : "Developer",
+	"contacts" : {
+		"mobile" : "04268122721",
+		"email" : "fernando_theotaku@hotmail.com",
+		"github" : "Fernando-Guerra",
+		"twitter" : "@itachi_guerra",
+		"location" : "Caracas"
+	},
+	"welcomeMessage" : "Bienvenido a mi Curriculum",
+	"skills" : [
+		"Programming ", "Database ", "Robotics "
+	],
+	"bioPic" : "images/fernando.jpg"
+};
+
+bio.city = "Catia";
+
+$("#main").append(bio.city); hasta aqui va el primero
+
+var work = {};
+
+work.jobPosition = "technical support";
+work.employer = "Cibersys";
+work.yearsWorked = "2 Months";
+work.city = "Caracas";
+
+var education = {};
+
+education["lastSchool"] = "IUJO";
+education["yearsSchool"] = "3 years";
+education["citySchool"] = "Caracas";
+
+$("#main").append(work["jobPosition"]);
+$("#main").append(education.lastSchool);
+
+*/
