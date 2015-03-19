b9b3112ecef1deb1ddd7bfa8cfe4e496394ecc52
Fernando-Guerra <fernando_theotaku@hotmail.com>
2015-03-19 15:49:00 -0430

    Ejercicio final JSON
    Hice el ejercicio final de JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -92,7 +92,7 @@ var education = {
 			"name": "Instituto Universitario Jesus Obrero",
 			"location": "Caracas",
 			"degree": "T.S.U. en Informatica",
-			"major": ["Informatica"],
+			"majors": ["Informatica"],
 			"dates": 2012,
 			"url": "www.iujo.com"
 		}
@@ -109,3 +109,85 @@ var education = {
 }
 */
 
+var bio = {
+	"name": "Fernando Cabezas",
+	"role": "Developer",
+	"contacts": {
+		"mobile": "04268122721",
+		"email": "Fernando_theotaku@hotmail.com",
+		"github": "Fernando_Guerra",
+		"twitter": "@itachi_guerra",
+		"location": "Caracas"
+	},
+	"welcomeMessage": "Curriculum de Fernando",
+	"skills": [
+		"programing", "database", "electronics"
+	],
+	"bioPic": "images/fernando.jpg"
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "Miguel Antonio Caro",
+			"location": "Caracas",
+			"degree": "Bachiller en Procesamiento de Datos",
+			"majors": ["Ciencias basicas", "Computacion"],
+			"dates": 2009,
+			"url": "www.miguelantonio.com" 
+		},
+		{
+			"name": "Instituto Universitario Jesus Obrero",
+			"location": "Caracas",
+			"degree": "T.S.U. en Informatica",
+			"majors": ["Informatica"],
+			"dates": 2012,
+			"url": "www.iujo.com"
+		}
+	],
+	
+	"onlineCourses":[
+		{
+			"title": "JavaScript Syntax",
+			"school": "Udacity",
+			"dates": 2015,
+			"url": "https://www.udacity.com/course/viewer#!/c-ud804/l-1930528550/e-1950678559/m-1936858588"
+		}
+	]
+}
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Loyalfeel",
+			"title": "Analista Integral de Sistemas",
+			"dates": "Marzo 2014 - Diciembre 2014",
+			"description": "Encargado de todas las areas de Informatica"
+		},
+		{
+			"employer": "Cibersys",
+			"title": "Analista Programador",
+			"dates": "Enero 2015 - Presente",
+			"description": "Programacion para crear nuevas soluciones de Comunicaciones"			
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Giroscopo",
+			"dates": "2013",
+			"descripcion": "Crear maquina voladora con nuevos diseños basados en los diseños del gran Ibrahim Lopez Garcia",
+			"images":[
+				"http://tatuy.net/web/wp-content/uploads/2012/03/nave-ibrahim2.jpg",
+				"http://1.bp.blogspot.com/-xgcXaUV2Cyk/UHxyg_4mEaI/AAAAAAAAAH0/MHxqKL74WNs/s1600/kkk0000ta7.jpg"
+			]
+		}
+	]
+}
+
+console.log(bio);
+console.log(education);
+console.log(work);
+console.log(projects);
